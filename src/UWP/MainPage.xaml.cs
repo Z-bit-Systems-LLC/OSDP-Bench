7550b15c2c8a000f72d1de337ad318c69ac9827d
Jonathan Horvath <jonathan@dd2consulting.com>
2020-03-15 20:55:54 -0400

    Verify that serial port can connect using OSDP.Net

--- src/UWP/MainPage.xaml.cs
+++ src/UWP/MainPage.xaml.cs
@@ -1,6 +1,15 @@
 ﻿
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+using Windows.UI.Xaml;
+using OSDP.Net;
+using OSDP.Net.Connections;
+
 namespace OSDPBenchUWP
 {
     /// <summary>
@@ -8,9 +17,29 @@ namespace OSDPBenchUWP
     /// </summary>
     public sealed partial class MainPage
     {
+        private ControlPanel _panel = new ControlPanel();
+        private Guid _connectionId;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var portNamesList = new List<string>();
+            foreach (var item in await DeviceInformation.FindAllAsync(SerialDevice.GetDeviceSelector()))
+            {
+                var serialDevice = await SerialDevice.FromIdAsync(item.Id);
+                portNamesList.Add(serialDevice.PortName);
+                serialDevice.Dispose();
+            }
+
+            if (portNamesList.Any())
+            {
+                _connectionId = _panel.StartConnection(new SerialPortOsdpConnection(portNamesList.First(), 9600));
+                _panel.AddDevice(_connectionId, 1, true, true);
+            }
         }
     }
 }
