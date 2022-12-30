a7b2f0b08e19fb5b29c3f6c9fa92741f6feafc63
Jonathan Horvath <qubit@z-bitco.com>
2022-12-30 08:59:20 -0500

    Fix linking issues for release mode

--- src/UI/Android/LinkerPleaseInclude.cs
+++ src/UI/Android/LinkerPleaseInclude.cs
@@ -1,89 +1,131 @@
 ﻿using Android.Views;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
 using System.Collections.Specialized;
 using System.Windows.Input;
+using MvvmCross;
 using MvvmCross.IoC;
-// ReSharper disable UnusedVariable
-// ReSharper disable RedundantAssignment
+using MvvmCross.Views;
 // ReSharper disable UnusedMember.Global
+// ReSharper disable RedundantAssignment
+// ReSharper disable UnusedVariable
 
-namespace OSDPBench.UI.Android;
-
-public class LinkerPleaseInclude
+namespace OSDPBench.UI.Android
 {
-    public void Include(Button button)
+    // This class is never actually executed, but when Xamarin linking is enabled it does how to ensure types and properties
+    // are preserved in the deployed app
+    [Preserve(AllMembers = true)]
+    public class LinkerPleaseInclude
     {
-        button.Click += (_, _) => button.Text = button.Text + "";
-    }
-
-    public void Include(CheckBox checkBox)
-    {
-        checkBox.CheckedChange += (_, _) => checkBox.Checked = !checkBox.Checked;
-    }
-
-    public void Include(Switch @switch)
-    {
-        @switch.CheckedChange += (_, _) => @switch.Checked = !@switch.Checked;
-    }
-
-    public void Include(View view)
-    {
-        view.Click += (_, _) => view.ContentDescription = view.ContentDescription + "";
-    }
-
-    public void Include(TextView text)
-    {
-        text.TextChanged += (_, _) => text.Text = "" + text.Text;
-        text.Hint = "" + text.Hint;
-    }
-
-    public void Include(CheckedTextView text)
-    {
-        text.TextChanged += (_, _) => text.Text = "" + text.Text;
-        text.Hint = "" + text.Hint;
-    }
-
-    public void Include(CompoundButton cb)
-    {
-        cb.CheckedChange += (_, _) => cb.Checked = !cb.Checked;
-    }
-
-    public void Include(SeekBar sb)
-    {
-        sb.ProgressChanged += (_, _) => sb.Progress = sb.Progress + 1;
-    }
-
-    public void Include(Activity act)
-    {
-        act.Title = act.Title + "";
-    }
-
-    public void Include(INotifyCollectionChanged changed)
-    {
-        changed.CollectionChanged += (_, e) =>
+        public void Include(Button button)
         {
-            var test = $"{e.Action}{e.NewItems}{e.NewStartingIndex}{e.OldItems}{e.OldStartingIndex}";
-        };
-    }
+            button.Click += (_, _) => button.Text = button.Text + "";
+        }
 
-    public void Include(ICommand command)
-    {
-        command.CanExecuteChanged += (_, _) =>
+        public void Include(CheckBox checkBox)
         {
-            if (command.CanExecute(null)) command.Execute(null);
-        };
-    }
+            checkBox.CheckedChange += (_, _) => checkBox.Checked = !checkBox.Checked;
+        }
 
-    public void Include(MvxPropertyInjector injector)
-    {
-        injector = new MvxPropertyInjector();
-    }
-
-    public void Include(System.ComponentModel.INotifyPropertyChanged changed)
-    {
-        changed.PropertyChanged += (_, e) =>
+        public void Include(Switch @switch)
         {
-            var test = e.PropertyName;
-        };
+            @switch.CheckedChange += (_, _) => @switch.Checked = !@switch.Checked;
+        }
+
+        public void Include(View view)
+        {
+            view.Click += (_, _) => view.ContentDescription = view.ContentDescription + "";
+        }
+
+        public void Include(TextView text)
+        {
+            text.AfterTextChanged += (_, e) => text.Text = $"{text.Text}{e.Editable}";
+            text.Hint = "" + text.Hint;
+        }
+
+        public void Include(CheckedTextView text)
+        {
+            text.AfterTextChanged += (_, _) => text.Text = $"{text.Text}";
+            text.Hint = "" + text.Hint;
+        }
+
+        public void Include(CompoundButton cb)
+        {
+            cb.CheckedChange += (_, _) => cb.Checked = !cb.Checked;
+        }
+
+        public void Include(SeekBar sb)
+        {
+            sb.ProgressChanged += (_, _) => sb.Progress = sb.Progress + 1;
+        }
+
+        public void Include(RadioGroup radioGroup)
+        {
+            radioGroup.CheckedChange += (_, args) => radioGroup.Check(args.CheckedId);
+        }
+
+        public void Include(RadioButton radioButton)
+        {
+            radioButton.CheckedChange += (_, args) => radioButton.Checked = args.IsChecked;
+        }
+
+        public void Include(RatingBar ratingBar)
+        {
+            ratingBar.RatingBarChange += (_, _) => ratingBar.Rating = 0 + ratingBar.Rating;
+        }
+
+        public void Include(Activity act)
+        {
+            act.Title = act.Title + "";
+        }
+
+        public void Include(INotifyCollectionChanged changed)
+        {
+            changed.CollectionChanged += (_, e) => { var test = $"{e.Action}{e.NewItems}{e.NewStartingIndex}{e.OldItems}{e.OldStartingIndex}"; };
+        }
+
+        public void Include(ICommand command)
+        {
+            command.CanExecuteChanged += (_, _) => { if (command.CanExecute(null)) command.Execute(null); };
+        }
+
+        public void Include(MvxPropertyInjector injector)
+        {
+            injector = new MvxPropertyInjector();
+        }
+
+        public void Include(System.ComponentModel.INotifyPropertyChanged changed)
+        {
+            changed.PropertyChanged += (_, e) =>
+            {
+                var test = e.PropertyName;
+            };
+        }
+
+        public void Include(MvxTaskBasedBindingContext context)
+        {
+            context.Dispose();
+            var context2 = new MvxTaskBasedBindingContext();
+            context2.Dispose();
+        }
+
+        public void Include(MvxNavigationService service, IMvxViewModelLoader loader, IMvxViewDispatcher dispatcher, IMvxIoCProvider iocProvider)
+        {
+            service = new MvxNavigationService(loader, dispatcher, iocProvider);
+        }
+
+        public void Include(ConsoleColor color)
+        {
+            Console.Write("");
+            Console.WriteLine("");
+            color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
     }
 }
-
