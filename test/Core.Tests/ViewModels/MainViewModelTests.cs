14b6252d8cc262765ac201ba36c1709f57a58ee1
Jonathan Horvath <qubit@z-bitco.com>
2024-03-03 20:49:20 -0500

    Attempt to fix build

--- test/Core.Tests/ViewModels/MainViewModelTests.cs
+++ test/Core.Tests/ViewModels/MainViewModelTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using Moq;
 using MvvmCross.Base;
 using MvvmCross.Navigation;
@@ -9,52 +10,53 @@ using OSDPBench.Core.Platforms;
 using OSDPBench.Core.Services;
 using OSDPBench.Core.ViewModels;
 
-namespace OSDPBench.Core.Tests.ViewModels
+namespace OSDPBench.Core.Tests.ViewModels;
+
+public class MainViewModelTests : MvxIoCSupportingTest
 {
-    public class MainViewModelTests : MvxIoCSupportingTest
+    protected MockDispatcher MockDispatcher { get; private set; }
+
+    protected override void AdditionalSetup()
     {
-        protected MockDispatcher MockDispatcher { get; private set; }
+        MockDispatcher = new MockDispatcher();
+        Ioc.RegisterSingleton<IMvxViewDispatcher>(MockDispatcher);
+        Ioc.RegisterSingleton<IMvxMainThreadAsyncDispatcher>(MockDispatcher);
+        
+        Ioc.RegisterSingleton(new Mock<ILoggerFactory>().Object);
+    }
 
-        protected override void AdditionalSetup()
-        {
-            MockDispatcher = new MockDispatcher();
-            Ioc.RegisterSingleton<IMvxViewDispatcher>(MockDispatcher);
-            Ioc.RegisterSingleton<IMvxMainThreadAsyncDispatcher>(MockDispatcher);
-        }
+    [Test]
+    public void MainViewModel_InitializedAvailableBaudRates()
+    {
+        Setup();
 
-        [Test]
-        public void MainViewModel_InitializedAvailableBaudRates()
-        {
-            Setup();
+        Ioc.RegisterSingleton(new Mock<IMvxNavigationService>().Object);
+        Ioc.RegisterSingleton(new Mock<IDeviceManagementService>().Object);
+        Ioc.RegisterSingleton(new Mock<ISerialPortConnection>().Object);
 
-            Ioc.RegisterSingleton(new Mock<IMvxNavigationService>().Object);
-            Ioc.RegisterSingleton(new Mock<IDeviceManagementService>().Object);
-            Ioc.RegisterSingleton(new Mock<ISerialPortConnection>().Object);
+        var mainViewModel = Ioc.IoCConstruct<RootViewModel>();
 
-            var mainViewModel = Ioc.IoCConstruct<RootViewModel>();
+        Assert.AreEqual(6, mainViewModel.AvailableBaudRates.Count);
+    }
 
-            Assert.AreEqual(7, mainViewModel.AvailableBaudRates.Count);
-        }
+    [Test]
+    public void MainViewModel_InitializedAvailableSerialPorts()
+    {
+        Setup();
 
-        [Test]
-        public void MainViewModel_InitializedAvailableSerialPorts()
-        {
-            Setup();
+        Ioc.RegisterSingleton(new Mock<IMvxNavigationService>().Object);
+        Ioc.RegisterSingleton(new Mock<IDeviceManagementService>().Object);
+        var serialPortConnection = new Mock<ISerialPortConnection>();
+        serialPortConnection.Setup(expression => expression.FindAvailableSerialPorts())
+            .ReturnsAsync(new[]
+            {
+                new AvailableSerialPort("id1", "test1", "desc1"), new AvailableSerialPort("id2", "test2", "desc2")
+            });
+        Ioc.RegisterSingleton(serialPortConnection.Object);
 
-            Ioc.RegisterSingleton(new Mock<IMvxNavigationService>().Object);
-            Ioc.RegisterSingleton(new Mock<IDeviceManagementService>().Object);
-            var serialPortConnection = new Mock<ISerialPortConnection>();
-            serialPortConnection.Setup(expression => expression.FindAvailableSerialPorts())
-                .ReturnsAsync(new[]
-                {
-                    new AvailableSerialPort("id1", "test1", "desc1"), new AvailableSerialPort("id2", "test2", "desc2")
-                });
-            Ioc.RegisterSingleton(serialPortConnection.Object);
+        var mainViewModel = Ioc.IoCConstruct<RootViewModel>();
+        mainViewModel.Prepare();
 
-            var mainViewModel = Ioc.IoCConstruct<RootViewModel>();
-            mainViewModel.Prepare();
-
-            Assert.AreEqual(2, mainViewModel.AvailableSerialPorts.Count);
-        }
+        Assert.AreEqual(2, mainViewModel.AvailableSerialPorts.Count);
     }
 }
