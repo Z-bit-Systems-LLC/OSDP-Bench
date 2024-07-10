bd3f0e28271df8db541569d177a89a5cf79655e9
Jonathan Horvath <qubit@z-bitco.com>
2024-07-10 13:43:57 -0400

    Add a progress ring to mange page

---
+++ src/UI/Windows/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OSDPBench.Windows.Converters
+{
+    internal class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            bool inverted = bool.Parse(parameter?.ToString() ?? "False");
+            return !inverted ? value != null ? Visibility.Visible : Visibility.Collapsed :
+                value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
