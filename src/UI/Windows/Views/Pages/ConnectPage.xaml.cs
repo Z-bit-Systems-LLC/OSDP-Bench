d4bc0aa0077599340975e684154639f1209791ee
Jonathan Horvath <byteme@bytedreamer.com>
2025-03-31 16:27:37 -0700

    Fix address selection not updating bindings

--- src/UI/Windows/Views/Pages/ConnectPage.xaml.cs
+++ src/UI/Windows/Views/Pages/ConnectPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using OSDPBench.Core.ViewModels.Pages;
+﻿using System.Windows.Controls;
+using OSDPBench.Core.ViewModels.Pages;
 using Wpf.Ui.Abstractions.Controls;
+using Wpf.Ui.Controls;
 
 namespace OSDPBench.Windows.Views.Pages;
 
@@ -27,4 +29,14 @@ public partial class ConnectPage : INavigableView<ConnectViewModel>
     public ConnectViewModel ViewModel { get; }
         
     public IEnumerable<string> ConnectionTypes => ["Discover", "Manual"];
+
+    private void AddressNumberBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        ViewModel.SelectedAddress = AddressNumberBox.Value ?? 0;
+    }
+
+    private void AddressNumberBox_OnValueChanged(object sender, NumberBoxValueChangedEventArgs args)
+    {
+        ViewModel.SelectedAddress = AddressNumberBox.Value ?? 0;
+    }
 }
